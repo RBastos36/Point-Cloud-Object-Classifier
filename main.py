ce9f6c00c65863cc9d78a20698081417476f21a1
RBastos36 <r.bastos@ua.pt>
2023-12-30 10:58:42 +0000

    Test

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 
-import glob
 import json
-from sklearn.model_selection import train_test_split
 from dataset import Dataset
 import torch
 from torchvision import transforms
@@ -22,17 +20,13 @@ def main():
     learning_rate = 0.001
     num_epochs = 50
 
-    # -----------------------------------------------------------------
-    # Create model
-    # -----------------------------------------------------------------
-    model = Model()
 
     # -----------------------------------------------------------------
     # Prepare Datasets
     # -----------------------------------------------------------------
 
+    # Reading from json file
     with open('dataset_filenames.json', 'r') as f:
-        # Reading from json file
         dataset_filenames = json.load(f)
 
     train_filenames = dataset_filenames['train_filenames']
@@ -41,8 +35,11 @@ def main():
     train_filenames = train_filenames[0:500]
     validation_filenames = validation_filenames[0:200]
 
-    print('Used ' + str(len(train_filenames)) + ' for training and ' + str(len(validation_filenames)) +
-          ' for validation.')
+    # While testing, use smaller datasets
+    train_filenames = train_filenames[0:1000]
+    validation_filenames = validation_filenames[0:200]
+
+    print(f'Used {len(train_filenames)} for training and {len(validation_filenames)} for validation.')
 
     train_dataset = Dataset(train_filenames)
     validation_dataset = Dataset(validation_filenames)
@@ -51,37 +48,18 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=batch_size, shuffle=True)
 
-    # Just for testing the train_loader
-    tensor_to_pil_image = transforms.ToPILImage()
-
-#     for batch_idx, (inputs, labels) in enumerate(train_loader):
-#         print('batch_idx = ' + str(batch_idx))
-#         print('inputs shape = ' + str(inputs.shape))
-#
-#         model.forward(inputs)
-#
-#         image_tensor_0 = inputs[0, :, :, :]
-#         print(image_tensor_0.shape)
-#
-#         image_pil_0 = tensor_to_pil_image(image_tensor_0)
-#         print(type(image_pil_0))
-#
-#         fig = plt.figure()
-#         plt.imshow(image_pil_0)
-#         plt.show()
-#
-#         exit(0)
 
     # -----------------------------------------------------------------
     # Train
     # -----------------------------------------------------------------
+    model = Model()
     trainer = Trainer(model=model,
                       train_loader=train_loader,
                       validation_loader=validation_loader,
                       learning_rate=learning_rate,
                       num_epochs=num_epochs,
                       model_path='models/checkpoint.pkl',
-                      load_model=False)
+                      load_model=True)
     trainer.train()
 
     plt.show()
